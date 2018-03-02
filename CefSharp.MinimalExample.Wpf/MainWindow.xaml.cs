07f8eb53f6c3020645ac3a3cb9206e248bccc6ae
Srinivasan <srini1978@gmail.com>
2018-03-02 14:36:24 +0530

    Update MainWindow.xaml.cs

--- CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -1,15 +1,24 @@
-
+using System;
+using System.Threading;
 using System.Windows;
-
+using CefSharp;
+using CefSharp.Wpf;
 namespace CefSharp.MinimalExample.Wpf
 {
     public partial class MainWindow : Window
     {
-        public MainWindow()
+       // ChromiumWebBrowser m_chromeBrowser = null;
+        private JavaScriptAdapter javaScriptCefAdapterObject;
+		private string questionnairePath;
+		public const string RenderProcessCrashedUrl = "http://test/resource/load";
+
+		public MainWindow()
         {
             InitializeComponent();
-              string questionnairePath = "file:///{0}HTMLResources/html/alpaca.html";
-		//	Browser.Load(page.ToString());
+			var handler = Browser.ResourceHandlerFactory as DefaultResourceHandlerFactory;
+			Browser.IsBrowserInitializedChanged += Browser_IsBrowserInitializedChanged;
+
+
 
 			if (Browser != null)
 			{
@@ -20,15 +29,59 @@ namespace CefSharp.MinimalExample.Wpf
 					return;
 				}
 
+				
+
 				questionnairePath = QuestionnairePreviewGenerator.RegisterResources(factory, "Questionnaire PReview");
 
-				//javaScriptCefAdapterObject = new JavaScriptAdapter(this);
-				//javaScriptCefAdapterObject.SetChromeBrowser(browser as ChromiumWebBrowser);
-				//if (!browser.IsBrowserInitialized)
-				//{
-				//	browser.RegisterJsObject("jscefAdapterObj", javaScriptCefAdapterObject);
-				//}
+				//Rajeev - Ucomment the following to reproduce the issue
+				//Browser.Load("file:///D:/Delivery/BDO/POC/CefSharp.MinimalExample-master/CefSharp.MinimalExample-master/Questionnaire_Preview.html");
+
+
+
 				Browser.Address = questionnairePath;
+				//Thread.Sleep(5000);
+
+				javaScriptCefAdapterObject = new JavaScriptAdapter();
+				javaScriptCefAdapterObject.SetChromeBrowser(Browser as ChromiumWebBrowser);
+
+				//Existing code is !Browser.IsBrowserInitialized
+				//	Browser.BrowserSettings.WebSecurity = CefState.Disabled;
+				if ( !Browser.IsBrowserInitialized)
+				{
+					Browser.RegisterJsObject("jscefAdapterObj", javaScriptCefAdapterObject);
+
+				}
+				else
+				{
+					
+				}
+				
+			}
+
+
+
+
+
+		}
+
+		
+		private void Browser_IsBrowserInitializedChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			if ((bool)e.NewValue == true)
+			{
+				//Browser.Load("www.google.com");
+				
+			}
+		}
+		void OnRenderProcessTerminated(IWebBrowser browserControl, IBrowser browser, CefTerminationStatus status)
+		{
+			browser.Reload(true);
+		}
+		public static string GetAppLocation()
+        {
+            return AppDomain.CurrentDomain.BaseDirectory;
         }
-    }
+		
+	
+	}
 }
